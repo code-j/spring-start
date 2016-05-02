ec54f84d43caaaee82f91a9f2f43e7bafa2af7ff
조우현 <woohyunjo@tmon.co.kr>
2016-05-03 08:52:08 +0900

    gitignore default setting

--- service_spring_project/src/main/java/com/myspring/ruby/HomeController.java
+++ service_spring_project/src/main/java/com/myspring/ruby/HomeController.java
@@ -1,16 +1,15 @@
 package com.myspring.ruby;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.gmon.coreruby.App;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles requests for the application home page.
@@ -33,7 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		model.addAttribute("serverName", new App().getName());
+        model.addAttribute("serverName", "service_spring_project");
+		//model.addAttribute("serverName", new App().getName());
 	
 
 		return "home";
